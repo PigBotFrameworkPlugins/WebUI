4f7778e4c6e5410defcb4b7cafdf498d40b06b1a
xzystudio <49107602+Gingmzmzx@users.noreply.github.com>
2024-08-17 07:13:36 +0800

    fix: use get to get the webui config

--- __init__.py
+++ __init__.py
@@ -55,12 +55,12 @@ def _enter():
         credentials: Annotated[HTTPBasicCredentials, Depends(security)],
     ):
         current_username_bytes = credentials.username.encode("utf8")
-        correct_username_bytes = config.plugins_config["webui"].get("basic_auth", {}).get("username", "admin").encode("utf8")
+        correct_username_bytes = config.plugins_config.get("webui", {}).get("basic_auth", {}).get("username", "admin").encode("utf8")
         is_correct_username = secrets.compare_digest(
             current_username_bytes, correct_username_bytes
         )
         current_password_bytes = credentials.password.encode("utf8")
-        correct_password_bytes = config.plugins_config["webui"].get("basic_auth", {}).get("password", "admin").encode("utf8")
+        correct_password_bytes = config.plugins_config.get("webui", {}).get("basic_auth", {}).get("password", "admin").encode("utf8")
         is_correct_password = secrets.compare_digest(
             current_password_bytes, correct_password_bytes
         )
